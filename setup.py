9b97ae716aead452a694c756463817057aaac27d
Andereoo <50446686+Andereoo@users.noreply.github.com>
2023-04-11 10:24:03 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="tkinterweb",
+    version="3.17.5",
+    description="HTML/CSS viewer for Tkinter - bindings for Tkhtml3",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andereoo/TkinterWeb",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+      ],
+    keywords="tkinter, Tkinter, tkhtml, Tkhtml, Tk, HTML, CSS, webbrowser",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pillow"],
+)

ae66ed098ee643b32635646009df97ba0ca17604
Andereoo <50446686+Andereoo@users.noreply.github.com>
2023-06-12 17:04:55 -0400

    Update __init__.py

--- tkinterweb/__init__.py
+++ tkinterweb/__init__.py
@@ -1,5 +1,5 @@
 """
-TkinterWeb v3.18
+TkinterWeb v3.19
 This is a wrapper for the Tkhtml3 widget from http://tkhtml.tcl.tk/tkhtml.html, 
 which displays styled HTML documents in Tkinter.
 
@@ -57,7 +57,7 @@ If you think this is a bug, please file a bug report at https://github.com/Ander
 
 __title__ = 'TkinterWeb'
 ___author__ = "Andereoo"
-__version__ = '3.18'
+__version__ = '3.19'
 __all__ = ['HtmlFrame', 'HtmlLabel', 'TkinterWeb', 'Demo']
 
 

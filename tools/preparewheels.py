f2db0484a8b6ecf288c7c103647df8a61ce426c8
Andrew <50446686+Andereoo@users.noreply.github.com>
2025-01-14 09:46:17 -0500

    Update version

--- tools/preparewheels.py
+++ tools/preparewheels.py
@@ -30,7 +30,7 @@ def get_platname():
 
 setup(
     name="tkinterweb",
-    version="3.24.12",
+    version="3.24.15",
     description="HTML/CSS viewer for Tkinter",
     long_description=README,
     long_description_content_type="text/markdown",

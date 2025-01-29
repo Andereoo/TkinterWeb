c2df83cd14ae2487a061ac49bcae77a55dbeb672
Andrew <50446686+Andereoo@users.noreply.github.com>
2025-01-29 11:24:36 -0500

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tkinterweb",
-    version="3.25.2",
+    version="3.25.3",
     description="HTML/CSS viewer for Tkinter",
     long_description=README,
     long_description_content_type="text/markdown",

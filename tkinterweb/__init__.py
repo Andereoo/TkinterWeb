006a8e9953fa14dcb729f1840dc65cccf231e1ca
Andrew <50446686+Andereoo@users.noreply.github.com>
2025-02-28 15:38:35 -0500

    Fix demo cursor

--- tkinterweb/__init__.py
+++ tkinterweb/__init__.py
@@ -45,7 +45,7 @@ class Demo():
         self.root = root = tk.Tk()
         self.frame = frame = HtmlFrame(root, messages_enabled=False)
         self.frame2 = HtmlFrame(root, on_navigate_fail=self.on_error)
-        self.button = tk.Button(root, cursor="pointer")
+        self.button = tk.Button(root, cursor="hand2")
 
         self.afters = []
         

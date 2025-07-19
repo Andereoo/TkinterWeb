c38282dfccfa0efa557d8fda6728cc195cf2348c
Andereoo <jx99zz@gmail.com>
2025-07-19 14:22:19 -0400

    Update version

--- tkinterweb/utilities.py
+++ tkinterweb/utilities.py
@@ -28,7 +28,7 @@ __title__ = 'TkinterWeb'
 __author__ = "Andereoo"
 __copyright__ = "Copyright (c) 2021-2025 Andereoo"
 __license__ = "MIT"
-__version__ = '4.4.0'
+__version__ = '4.4.1'
 
 
 ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "resources")
